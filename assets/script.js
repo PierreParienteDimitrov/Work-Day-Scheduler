95b05dd9db9283d7050a2eb2f97e3f44cf68fa36
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-08-07 11:37:31 -0700

    recreated logic with a for loop instead of foreach. Added local storage.

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,78 @@
-for(i = 1; i <=24 ; i++) {
-    var calendarItem = $('<div>').addClass('d-flex calendar-item align-items-center')
-    var timeDiv = $('<div>').addClass('time')
-    if(i < 12) {
-        var timeSlot = $('<p>').text(i + " AM")
-    } else {
-        var timeSlot = $('<p>').text(i + " PM")
-    }
+var timeSlots = [
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+  "8pm",
+  "9pm",
+  "10pm",
+  "11pm",
+  "12am",
+];
 
-    // var task = $('<div>').addClass('task')
-    var textarea = $('<textarea>')
-    var button = $('<button>').addClass('save')
+console.log(timeSlots);
 
-    button.text('Button')
-    $('.container').append(calendarItem)
-    calendarItem.append(timeDiv, textarea, button)
-    timeDiv.append(timeSlot)
+for (i = 0; i < timeSlots.length; i++) {
+  
+  // get local storage on load
+  var storedInput = localStorage.getItem(timeSlots[i]);
+  
+  // creating html elements
+  var newRow = $("<div>").addClass("row new-row");
+  var hour = $("<div>").addClass("col-md-1 col-sm-12 hour");
+  // var inputDiv = $("<div>").addClass("col-md-10 col-sm-12 inputDiv");
+  var input = $("<input>").addClass("input col-md-10 col-sm-12");
+  input.attr("data-value", timeSlots[i]);
+  hour.text(timeSlots[i]);
 
-    var currentTime = parseInt(moment().format('h'))
-    console.log(typeof(currentTime))
-    
-    // if(currentTime > i) {
-    //     calendarItem.addClass('grey-bg')
-    //     calendarItem.removeClass('red-bg')
-    //     calendarItem.removeClass('green-bg')
+  // adding local storage to input
+    input.val(storedInput);
 
-    // } else if(currentTime = i) {
-    //     calendarItem.addClass('red-bg')
-    //     calendarItem.removeClass('grey-bg')
-    //     calendarItem.removeClass('green-bg')
-    // } else {
-    //     calendarItem.addClass('green-bg')
-    //     calendarItem.removeClass('grey-bg')
-    //     calendarItem.removeClass('red-bg')
-    // }
+  // current time
+  var currentTime = parseInt(moment().format("H"));
 
+  // setting background colour
+  if (i + 9 < currentTime) {
+    newRow.addClass("grey-bg");
+  } else if (i + 9 === currentTime) {
+    newRow.addClass("green-bg");
+  } else {
+    newRow.addClass("red-bg");
+  }
+
+  var btn = $("<button>", {
+    type: "button",
+    value: timeSlots[i],
+    on: {
+      click: function saveToLocal() {
+        var currentEl = $("input[data-value='" + this.value + "']");
+        // console.log(currentEl.val())
+        var selectedHour = currentEl.data("value");
+        // console.log(currentEl)
+        // console.log(typeof(currentEl))
+        // console.log(selectedHour)
+        if (currentEl) {
+          var inputText = currentEl.val();
+          localStorage.setItem(selectedHour, inputText);
+        }
+      },
+    },
+  });
+
+  btn.addClass("col-md-1 col-sm-12 text-center button-save");
+
+  // adding html elements to the page
+  $(".calendar").append(newRow);
+  newRow.append(hour);
+  newRow.append(input);
+  // textInput.append(task);
+  newRow.append(btn);
 }
+
